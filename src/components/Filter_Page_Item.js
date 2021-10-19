fb52086b203452c01cd26f37d474bc3c06d347ff
Hassnain-bit <m.hassnainkhan32@gmail.com>
2021-10-19 19:46:04 +0500

    Filter-page-restaurent-item

--- src/components/Filter_Page_Item.js
+++ src/components/Filter_Page_Item.js
@@ -1,4 +1,4 @@
-import '../styles/CuisinesItem.css'
+import '../styles/Filter_Page_Item.css'
 import restaurentImage from '../images/restaurant.jpg'
 import bookmarkIcon from '../images/bookmark.png'
 
@@ -8,30 +8,29 @@ export default function Filter_Page_Item() {
         <div>
            
             
-           <div className="box-restaurent-2nd-page">
+           <div className="filter-page-box">
 
                 
                             {/* img-box */}
-                            <div className="slide-img-restaurent-2nd-page">
+                            <div className="filter-P-restaurent-image">
                             <img alt="restaurent-pic" src={restaurentImage}/>
                             
                             {/* rating */}
-                        <div className="over-rating-2nd-page">	
-                            <h6 href="#" className="buy-btn-restaurent-2nd-page">4.5</h6>
+                        <div className="filter-P-rating-box">	
+                            <h6 href="#" className="filter-P-rating">4.5</h6>
                             </div>
                             </div>
                             {/* detail-box */}
-                            <div className="detail-box-restaurent-2nd-page">
+                            <div className="filter-P-restaurent-detail-box">
                             {/* restaurent-name */}
-                            <div className="type-restaurent-2nd-page">
+                            <div className="filter-P-restaurent-name">
                             <a href="#abc">JW Kitchen</a>
-                            <span className="margao-details-2nd-page">JW Marriott Hotel, Vital Mallya Road, Central Multi-Cuisne, North 
-                                ₹ 2,500 for 2 | Multi-Cuisine, North Indian</span>
-                            <span className="flat-off-2nd-page">1 offer avaiable</span>
+                            <span className="filter-P-restaurent-location">JW Marriott Hotel, Vital Mallya Road, Central</span>
+                            <span className="filter-P-offer">1 offer avaiable</span>
 
                             <div style={{marginTop: "5px"}}>
                                 <span><img alt="bookmark-icon" src={bookmarkIcon} height="13px" width="13px"/></span>
-                                <span className="text-booked">Booked 1 time today</span>
+                                <span className="filter-P-text-booked">Booked 1 time today</span>
                             </div>
                             </div>
                                 
