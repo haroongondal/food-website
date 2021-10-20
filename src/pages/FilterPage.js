1441162405c57b1447e31879ba96bd9c1dcb6ebf
Hassnain-bit <m.hassnainkhan32@gmail.com>
2021-10-20 15:02:47 +0500

    bread

--- src/pages/FilterPage.js
+++ src/pages/FilterPage.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import Carousel from 'react-multi-carousel'
 import Breadcrumb from '../components/Breadcrumb'
+import RestaurentItem from '../components/RestaurentItem'
+import CarouselRestaurent from '../styles/Carousel_Restaurent';
+import '../styles/FilterPage.css'
 
 function FilterPage() {
     return (
         <div>
             <Breadcrumb/>
+            
         </div>
     )
 }
