c6d9b1ccec748f3066ab52cf922178297ebc0ec4
Abdullah000001 <geniusswork00@gmail.com>
2021-10-22 10:58:24 +0500

    changin the tabs

--- src/components/SectionFollower.js
+++ src/components/SectionFollower.js
@@ -2,24 +2,28 @@ import React, { useState } from 'react'
 import '../styles/FollowerItems.css'
 import FollowerItem from '../components/FollowerItem';
 function SectionFollower() {
-    const [isOpened, setIsOpened] = useState(true);
-
+    const [activeTab, setActiveTab] = useState("tab1");
+    const handleTab1 = () => {
+       
+        setActiveTab("tab1");
+      };
+      const handleTab2 = () => {
+       
+        setActiveTab("tab2");
+      };
     return (
         <div>
-            <div class="right-section" style={{backgroundColor: "white"}}>
+            <div className="right-section" style={{backgroundColor: "white"}}>
                 <div style={{textAlign: "center",margin: "20px",marginTop: "30px"}}>
                     <h3 style={{ fontSize:"1.4rem",
                     fontWeight: "700"}}>Followers</h3>
                 </div>
-                <ul class="tab-group">
-                      <li class="tab active"><a  onClick={()=>setIsOpened(isOpened)}>Followers</a></li>
-                      <li class="tab"><a>Following</a></li>
+                <ul className="tab-group">
+                      <li className={`tab active ${activeTab === "tab1" ? "active" : ""}`}  onClick={handleTab1}><a>Followers</a></li>
+                      <li className={`tab ${activeTab === "tab2" ? "active" : ""}`}  onClick={handleTab2}><a>Following</a></li>
                 </ul>
-                {isOpened && (
-       <FollowerItem/>
-      )}
-
-                
+             
+                {activeTab === "tab1" ? <FollowerItem/> : <h1>Hassnain Khan </h1>}
             
             </div>
         </div>
