3290cb89d5e8bdb462f2b79aff3666ceb9f3ee8d
Hassnain-bit <m.hassnainkhan32@gmail.com>
2021-11-11 16:43:34 +0500

    Merge conflicts solved

--- src/pages/FilterPage.js
+++ src/pages/FilterPage.js
@@ -48,49 +48,6 @@ function FilterPage() {
   const closeFiltersPop = () => {
     setFiltersShowing(false);
   };
-<<<<<<< HEAD
-
-  const [url, seturl] = useState(
-    `https://api.masairapp.com/api/Restaurant/GetRestaurantsByFilters?cusineId=&RestaurantId=&featureId=&tagId=&offset=0&limit=10`
-  );
-  const [tagIds, setTagIds] = useState([]);
-  const [cusineIds, setCusineIds] = useState([]);
-  const [featureIds, setFeatureIds] = useState([]);
-  const { data, isPending, error } = useFetch(url);
-
-  const handleFilters = (isChecked, filterType, id) => {
-    if (Types.FilterTypes.QuickFilters === filterType) {
-    }
-
-    if (Types.FilterTypes.CusineFilters === filterType) {
-      if (isChecked) {
-        setCusineIds((oldArray) => [...oldArray, id]);
-      } else {
-        const index = cusineIds.findIndex((id_) => id_ === id);
-        if (index !== -1) {
-          setCusineIds([
-            ...cusineIds.slice(0, index),
-            ...cusineIds.slice(index + 1),
-          ]);
-        }
-      }
-    }
-
-    if (Types.FilterTypes.FeatureFilters === filterType) {
-    }
-
-    if (Types.FilterTypes.TagFilters === filterType) {
-      if (isChecked) {
-        setTagIds((oldArray) => [...oldArray, id]);
-      } else {
-        const index = tagIds.findIndex((id_) => id_ === id);
-        if (index !== -1) {
-          setTagIds([...tagIds.slice(0, index), ...tagIds.slice(index + 1)]);
-        }
-      }
-    }
-  };
-=======
   
     const [url, seturl] = useState(`https://api.masairapp.com/api/Restaurant/GetRestaurantsByFilters?cusineId=&RestaurantId=&featureId=&tagId=&offset=0&limit=10`);
     const [tagIds, setTagIds] = useState([])
@@ -136,7 +93,7 @@ function FilterPage() {
                             }
                 }
             }
-            
+        }     
 
         
        
@@ -144,7 +101,7 @@ function FilterPage() {
        setSort(sort)
     }
  
->>>>>>> 6c6ca4af6e80475c5c97d0cb21afbf9c512b7cd1
+
 
   useEffect(() => {
     seturl(
@@ -152,32 +109,32 @@ function FilterPage() {
     );
   }, [cusineIds, featureIds, tagIds]);
 
-  const handleSorting = (sort) => {
-    if (!isPending && !error && data) {
-      let n = data.ListOfRestaurant.length;
-      if (n > 1) {
-        if (Types.SortType.Popularity === sort) {
-          console.log("sorttttttttttttttttttttttt => " + sort);
-          data.ListOfRestaurant.sort((a, b) => b.CostOfTwo - a.CostOfTwo);
-        }
+//   const handleSorting = (sort) => {
+//     if (!isPending && !error && data) {
+//       let n = data.ListOfRestaurant.length;
+//       if (n > 1) {
+//         if (Types.SortType.Popularity === sort) {
+//           console.log("sorttttttttttttttttttttttt => " + sort);
+//           data.ListOfRestaurant.sort((a, b) => b.CostOfTwo - a.CostOfTwo);
+//         }
 
-        if (Types.SortType.Ratings === sort) {
-          console.log("sorttttttttttttttttttttttt => " + sort);
-          data.ListOfRestaurant.sort((a, b) => b.CostOfTwo - a.CostOfTwo);
-        }
+//         if (Types.SortType.Ratings === sort) {
+//           console.log("sorttttttttttttttttttttttt => " + sort);
+//           data.ListOfRestaurant.sort((a, b) => b.CostOfTwo - a.CostOfTwo);
+//         }
 
-        if (Types.SortType.HighPrice === sort) {
-          console.log("sorttttttttttttttttttttttt => " + sort);
-          data.ListOfRestaurant.sort((a, b) => b.CostOfTwo - a.CostOfTwo);
-        }
+//         if (Types.SortType.HighPrice === sort) {
+//           console.log("sorttttttttttttttttttttttt => " + sort);
+//           data.ListOfRestaurant.sort((a, b) => b.CostOfTwo - a.CostOfTwo);
+//         }
 
-        if (Types.SortType.LowPrice === sort) {
-          console.log("sorttttttttttttttttttttttt => " + sort);
-          data.ListOfRestaurant.sort((a, b) => a.CostOfTwo - b.CostOfTwo);
-        }
-      }
-    }
-  };
+//         if (Types.SortType.LowPrice === sort) {
+//           console.log("sorttttttttttttttttttttttt => " + sort);
+//           data.ListOfRestaurant.sort((a, b) => a.CostOfTwo - b.CostOfTwo);
+//         }
+//       }
+//     }
+//   };
 
   return (
     <div>
@@ -337,10 +294,7 @@ function FilterPage() {
           </ForDevice>
 
           <ForDevice deviceName={["tablet", "desktop"]}>
-<<<<<<< HEAD
-=======
-           
->>>>>>> 6c6ca4af6e80475c5c97d0cb21afbf9c512b7cd1
+
             <div className="adjust">
               <div className="alignment listing_sidebar">
                 <FiltersItem
@@ -360,25 +314,16 @@ function FilterPage() {
                   handleFilters={handleFilters}
                 />
               </div>
-<<<<<<< HEAD
+
             </div>
           </ForDevice>
-=======
 
-</div>
-
-</ForDevice>
-
-
-                                <div className="right-section">
-                    <div className="div-top-boxes-content">
->>>>>>> 6c6ca4af6e80475c5c97d0cb21afbf9c512b7cd1
 
           <div className="right-section">
             <div className="div-top-boxes-content">
               <HeadingFilterPage />
               <Sortby getSortType={handleSorting} />
-              {/* <div className="align-boxes">
+              <div className="align-boxes">
                     {isPending && <div><Skeleton width={200} height={250}orientation={"horizontal"}/></div>}
                     {error && <div>{error}</div>}
                     {sort === Types.SortType.HighPrice && data && data.ListOfRestaurant.sort((a, b) => 
@@ -394,16 +339,10 @@ function FilterPage() {
                     (b.AverageRating - a.AverageRating)).map((rest) => <FilterPageItem key = {rest.Id} data = {rest}/>) }
                     
                     {sort === "-1" && data && data.ListOfRestaurant.map((rest) => <FilterPageItem key = {rest.Id} data = {rest}/>)}
-<<<<<<< HEAD
-                    </div> */}
-            </div>
-=======
+
                     </div>
-                    
+            </div>
 
-
-          
->>>>>>> 6c6ca4af6e80475c5c97d0cb21afbf9c512b7cd1
             <hr className="grey" />
             <Pagination />
           </div>
